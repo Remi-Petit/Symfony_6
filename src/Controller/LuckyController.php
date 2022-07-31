4b5e63b58e6baab1226a9b6112c84b0bc4860a20
Remi <remi.petit93370@gmail.com>
2022-07-31 17:04:28 +0200

    apprentissage sass

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -7,12 +7,23 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class LuckyController extends AbstractController {
+
+    /** * @Route("/") */
+    public function home(): Response {
+        return $this->render('pages/home.html.twig');
+    }
+
     /** * @Route("/lucky/number") */
     public function number(): Response {
         $number = random_int(0, 100);
 
-        return $this->render('lucky/number.html.twig', [
+        return $this->render('pages/number.html.twig', [
             'number' => $number,
         ]);
+    }
+
+    /** * @Route("/produit") */
+    public function yolo(): Response {
+        return $this->render('pages/produit.html.twig');
     }
 }
